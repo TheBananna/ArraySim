aa3b1dcd0b0de85e46e4b5bb96537b947326006e
TheBanana <nickmaster2222@gmail.com>
2023-09-25 16:57:49 -0400

    changes

---
+++ ArraySimPy/constants.py
@@ -0,0 +1,9 @@
+import numpy as np
+EM_DATA = {
+#"vacuum_speed_of_light":3.e8, 
+"vacuum_speed_of_light":2.99792458e8, 
+"c0":2.99792458e8, 
+"eps0": 8.8541878176203898505e-12, 
+"mu0": 4.0e-7*np.pi,
+"eta0":np.sqrt(4.0e-7*np.pi/8.853e-12)}
+#EM_constants["c0"] = 1.0/np.sqrt(EM_constants["eps0"] * EM_constants["mu0"])
