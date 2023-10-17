a3e3b95784ceae89b88912b00392a895070537a5
TheBanana <nickmaster2222@gmail.com>
2023-10-17 13:05:00 -0400

    changes

--- ArraySim/ArraySim.cpp
+++ ArraySim/ArraySim.cpp
@@ -17,19 +17,26 @@ int main()
     float wavelength = c0 / frequency;
     Array array;
     Isotrope a1;
+    a1.set_phase(2.5);
+    Isotrope a2;
+    a2.set_phase(5);
+    Isotrope a3;
+    a3.set_phase(7.5);
+    Isotrope a4;
+    a4.set_phase(10);
     //array.add_antenna(&a1, -wavelength / 4, 0, -wavelength / 4);
     //array.add_antenna(&a1, -wavelength / 4, 0, wavelength / 4);
     //array.add_antenna(&a1, wavelength / 4, 0, -wavelength / 4);
     //array.add_antenna(&a1, wavelength / 4, 0, wavelength / 4);
 
     array.add_antenna(&a1, -3 * wavelength / 4, 0, 0);
-    array.add_antenna(&a1, -wavelength / 4, 0, 0);
-    array.add_antenna(&a1, wavelength / 4, 0, 0);
-    array.add_antenna(&a1, 3 * wavelength / 4, 0, 0);
+    array.add_antenna(&a2, -wavelength / 4, 0, 0);
+    array.add_antenna(&a3, wavelength / 4, 0, 0);
+    array.add_antenna(&a4, 3 * wavelength / 4, 0, 0);
 
 
 
-    auto results = array.simulate(frequency, 2, 1000);
+    auto results = array.simulate(frequency, 2, 60);
     for (float f : results[0])
     {
         std::cout << f << std::endl;
